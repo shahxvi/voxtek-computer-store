890cf6d5fe088c46536ff0d075852e084df05892
shahxvi <2025171523@student.uitm.edu.my>
2025-12-17 23:38:17 +0800

    Created Admin class

---
+++ src/Admin.java
@@ -0,0 +1,54 @@
+public class Admin /* extends User */ {
+    private int id;
+    private String password;
+
+    /* Contructors */
+    public Admin() {
+        // super();
+        id = 0;
+        password = "";
+    }
+
+    public Admin(String name, int phoneNumber, int id, String password) {
+        // super(name, phoneNumber);
+        this.id = id;
+        this.password = password;
+    }
+
+    public Admin(Admin other) {
+        // super(admin);
+        this.id = other.id;
+        this.password = other.password;
+    }
+    /* Contructors */
+
+    /* Setters */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    /* Setters */
+
+    /* Getters */
+    public int getId() {
+        return id;
+    }
+    /* Getters */
+
+    public boolean verifyID(int inputID) {
+        if (id == inputID) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean verifyPassword(String inputPassword) {
+        if (password.equalsIgnoreCase(inputPassword)) {
+            return true;
+        }
+        return false;
+    }
+}
