38119b0444cc428650f548df1441b8590b07b6e7
relatableandgoofy69 <fraden400@gmail.com>
2025-12-18 00:34:38 +0800

    add user class

--- src/User.java
+++ src/User.java
@@ -1,6 +1,27 @@
 // MIT License
 // Copyright (c) 2025 Raden
 
-public class User {
+public abstract class User {
+    String name;
+    int phoneNumber;
 
+    protected void setName(String name) {
+        this.name = name;
+    }
+
+    protected void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String toString() {
+        return name + " " + phoneNumber;
+    }
 }
