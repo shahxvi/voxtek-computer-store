8d6126042d7c2d3692cf1952f420bc5a3788e8bf
shahxvi <2025171523@student.uitm.edu.my>
2026-01-06 13:32:55 +0800

    Create ShoppingCart

---
+++ src/ShoppingCart.java
@@ -0,0 +1,34 @@
+// MIT License
+// Copyright (c) 2025 Shah
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+    private ArrayList<Product> products = new ArrayList<>();
+
+    public void addProducts(Product[] products) {
+        for (int i = 0; i < products.length; i++) {
+            this.products.add(products[i]);
+        }
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public Product getProduct(int index) {
+        return products.get(index);
+    }
+
+    public Product[] getProducts() {
+        Product[] allProducts = new Product[products.size()];
+        for (int i = 0; i < allProducts.length; i++) {
+            allProducts[i] = products.get(i);
+        }
+        return allProducts;
+    }
+}
